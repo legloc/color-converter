e66885b70b48ca50678259a1650d3dd9e2597967
legloc <aliiev.akhtem@gmail.com>
2021-07-26 23:16:27 +0300

    Removed input masks. Created basic functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,92 @@
+import { useState } from 'react'
+import convert from 'color-convert'
 import Logo from './components/Logo'
 import FormGroup from './components/FormGroup'
 import './App.sass'
 
 const App = () => {
+  const [state, setState] = useState({
+    hex: '',
+    rgb: '',
+    hsl: '',
+    cmyk: ''
+  })
+
+  const maskTo = (format, value) => {
+    if (format === 'hex') return `#${ value }`
+    if (format === 'rgb') return `rgb(${ value })`
+    if (format === 'hsl') return `hsl(${ value[0] },${ value[1] }%,${ value[2] }%)`
+    if (format === 'cmyk') return `cmyk(${ value })`
+  }
+
+  const handleHEXchange = e => {
+    setState({
+      hex: e,
+      rgb: maskTo('rgb', convert.hex.rgb(e)),
+      hsl: maskTo('hsl', convert.hex.hsl(e)),
+      cmyk: maskTo('cmyk', convert.hex.cmyk(e))
+    })
+  }
+
+  const handleRGBchange = e => {
+    let substring = e.substring(e.indexOf('(') + 1, e.indexOf(')'))
+    let value = substring.replaceAll(' ', '').split(',')
+    
+    setState({
+      hex: maskTo('hex', convert.rgb.hex(value)),
+      rgb: e,
+      hsl: maskTo('hsl', convert.rgb.hsl(value)),
+      cmyk: maskTo('cmyk', convert.rgb.cmyk(value))
+    })
+  }
+
+  const handleHSLchange = e => {
+    let substring = e.substring(e.indexOf('(') + 1, e.indexOf(')')).replaceAll('%', '')
+    let value = substring.split(', ')
+
+    setState({
+      hex: maskTo('hex', convert.hsl.hex(value)),
+      rgb: maskTo('rgb', convert.hsl.rgb(value)),
+      hsl: e,
+      cmyk: maskTo('cmyk', convert.hsl.cmyk(value))
+    })
+  }
+
+  const handleCMYKchange = e => {
+    let substring = e.substring(e.indexOf('(') + 1, e.indexOf(')'))
+    let value = substring.split(', ')
+
+    setState({
+      hex: maskTo('hex', convert.cmyk.hex(value)),
+      rgb: maskTo('rgb', convert.cmyk.rgb(value)),
+      hsl: maskTo('hsl', convert.cmyk.hsl(value)),
+      cmyk: e
+    })
+  }
+
   return (
     <div className="wrapper">
-      <Logo color="#ccc" />
+      <Logo color={ state.hex } />
       <FormGroup
         title="HEX"
         placeholder="#FFFFFF"
-        mask="#******" />
+        value={ state.hex }
+        onChange={ e => handleHEXchange(e) } />
       <FormGroup
         title="RGB"
-        placeholder="rgb(255, 255, 255)"
-        mask="rgb(999, 999, 999)" />
+        placeholder="rgb(255,255,255)"
+        value={ state.rgb }
+        onChange={ e => handleRGBchange(e) } />
       <FormGroup
         title="HSL"
-        placeholder="hsl(0, 0%, 100%)"
-        mask="hsl(999, 999%, 999%)" />
+        placeholder="hsl(0,0%,100%)"
+        value={ state.hsl }
+        onChange={ e => handleHSLchange(e) } />
       <FormGroup
         title="CMYK"
-        placeholder="cmyk(0, 0, 0, 0)"
-        mask="cmyk(999, 999, 999, 999)" />
+        placeholder="cmyk(0,0,0,0)"
+        value={ state.cmyk }
+        onChange={ e => handleCMYKchange(e) } />
     </div>
   )
 }
--- src/components/FormGroup.js
+++ src/components/FormGroup.js
@@ -1,17 +1,17 @@
-import InputMask from 'react-input-mask'
 import IconCopy from '../icon-copy.svg'
 
-const FormGroup = ({ title, placeholder, mask }) => {
+const FormGroup = ({ title, placeholder, value, onChange }) => {
   return (
     <div className="form-group">
       <label className="form-label" htmlFor={ `${ title }-input` }>{ title }</label>
       <div className="input-group">
-        <InputMask
+        <input
           className="form-input"
           id={ `${ title }-input` }
           type="text"
-          mask={ mask }
-          placeholder={ placeholder }/>
+          placeholder={ placeholder }
+          value={ value }
+          onChange={ e => onChange(e.target.value) } />
         <button className="input-addon">
           <img className="addon-icon" src={ IconCopy } alt="Copy" />
         </button>
